fcb17dc1e73abe8a79d7bd8eb3a6a692bbed770e
baoyx007 <baoyx007@gmail.com>
2020-05-25 22:16:10 +0800

    add docker file

---
+++ scripts/docker-run.sh
@@ -0,0 +1,2 @@
+docker build -t devops .
+docker run -d -v /webstatics/blog:/data --name devops -p 3000:3000 -d devops
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api/devops')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/blog')
   getHello(): string {
     return this.appService.getHello();
   }
